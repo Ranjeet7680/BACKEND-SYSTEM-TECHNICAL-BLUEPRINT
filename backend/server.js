6fa7e384991e49a8222c009e410a12ff23beb9a6
Ranjeet kumar <157788421+Ranjeet7680@users.noreply.github.com>
2025-12-23 18:20:19 +0530

    Change server port from 5000 to 3000

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,12 @@
-// 🔴 MUST BE FIRST
 require("dotenv").config();
 
 const http = require("http");
 const app = require("./src/app");
 const { initSocket } = require("./src/sockets/socket");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
-
-// initialize socket
 initSocket(server);
 
 server.listen(PORT, () => {
